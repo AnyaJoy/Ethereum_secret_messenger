2bd6a55fbeefa8d34b85d3d4ad25c3323dc4582c
AnyaJoy <urda-a@mail.ru>
2023-04-18 13:46:13 +0300

    truffle

--- mini-project/index.js
+++ mini-project/index.js
@@ -13,7 +13,8 @@ async function main() {
     if (typeof web3 !== 'undefined') {
         web3 = new Web3(ethereum);
     } else {
-        web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"));
+        web3 = new Web3(new Web3.providers.HttpProvider("https://goerli.infura.io/v3/0d3314a63ac644e8874a4b614f5ec10f"));
+        // web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"));
     }
 
     web3.eth.defaultAccount = web3.eth.accounts[0];
@@ -49,7 +50,8 @@ async function main() {
           type: "function",
         },
       ],
-      "0x85e848C49F05C311EBfd3b4479917A5Fd12e188f", //contract address
+      "0x50D05467Ce89a76f40b9a079DB15a1cD43ABc188",
+    //   "0x85e848C49F05C311EBfd3b4479917A5Fd12e188f", //contract address
       { from: "0xD777921c7469De83E6c2875541233a8F416d0375" } //sender address
     );
 
